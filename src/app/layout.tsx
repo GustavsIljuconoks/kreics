20601facabfb8ff0ca4190c3b0f5b7f7b61a922e
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2024-07-03 19:02:49 +0300

    setup firebase && about page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kreics",
+  title: {
+    template: '%s | Kreics',
+    default: 'Kreics',
+  },
   description: "Portfolio website showcasing various works by Kreics",
 };
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div id="content" className="w-full min-h-screen px-8 py-20 sm:p-lg md:px-16 md:py-20 xl:px-32">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,61 @@
+"use client";
+
+import Image from "next/image";
 import NavBar from "@/ui/nav-bar";
 
-export default function Home() {
+import { db } from "@/utils/firebase/config";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    const fetchImageUrl = async () => {
+      const docRef = doc(db, "test", "index");
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setImageUrl(docSnap.data().imageUrl);
+      } else {
+        console.log("No such document!");
+      }
+    };
+
+    fetchImageUrl();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div
-        id="content"
-        className="w-full px-8 py-20 sm:p-lg md:px-16 md:py-20 xl:px-32"
-      >
-        <div className="lg:flex w-full">
-          <NavBar />
+    <main className="flex flex-col p-6">
+      <div className="lg:flex w-full">
+        <NavBar />
 
-          <div className="lg:w-5/6 text-center mt-16">
-            <h1 className="text-orange text-6xl lg:text-8xl font-bold mb-4">
-              KREICS
-            </h1>
+        <div className="lg:w-5/6 text-center mt-16">
+          <h1 className="text-orange text-6xl lg:text-8xl font-bold mb-4">
+            KREICS
+          </h1>
 
-            <h4 className="text-black-70 text-md lg:text-2xl font-bold mb-8">
-              portraying a feeling
-            </h4>
+          <h4 className="text-black-70 text-md lg:text-2xl font-bold mb-8">
+            portraying a feeling
+          </h4>
 
-            <p className="text-black-40 font-semibold text-md lg:text-2.5xl">
-              check out my work{" "}
-              <a href="/works" className="underline hover:text-black-70">
-                here
-              </a>
-              .
-            </p>
+          <div id="photo-box" className="flex justify-center mb-6">
+            <Image
+              src={imageUrl}
+              alt="photo of kreics"
+              width={1000}
+              height={100}
+              className="my-4"
+            />
           </div>
+
+          <p className="text-black-40 font-semibold text-md lg:text-2.5xl">
+            check out my work{" "}
+            <a href="/works" className="underline hover:text-black-70">
+              here
+            </a>
+            .
+          </p>
         </div>
       </div>
     </main>
