387133f153cf1ce3155af3baa8e5bf9420084588
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2025-01-11 15:51:56 +0200

    try to set up cloudinary plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_CLOUD_NAME"),
+      api_key: env("CLOUDINARY_API_KEY"),
+      api_secret: env("CLOUDINARY_API_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
+    },
+  },
+});
