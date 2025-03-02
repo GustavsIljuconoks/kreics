f8eface63d3c4c27a1d0efdada3f46c1cbfdaa47
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2025-03-02 19:50:47 +0200

    update database connection

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "kreics_db"),
-      user: env("DATABASE_USERNAME", "admin"),
+      user: env("DATABASE_USER", "root"),
       password: env("DATABASE_PASSWORD", "pass"),
       ssl: env.bool("DATABASE_SSL", false),
     },
